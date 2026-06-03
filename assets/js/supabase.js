839e39bd6259c4680f136628a5bf9a79721d6407
ZaynDHR <daher.zein12221@gmail.com>
2026-06-03 15:28:07 +0200

    move keys to env

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://ciofnfshfibetkhmbtem.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_SM4875Qb6rcmOoKeKvUQhw_wfRBCnPq';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
